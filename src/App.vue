989de9965294e5c9600630e7a9a5c43ed5548d81
Gabriele <gabriele.palma.bool@gmail.com>
2023-12-21 17:07:41 +0100

    import img files

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,8 @@ export default {
 </template>
 
 <style lang="scss">
-@use './styles/partials/variables' as *;
-@use './styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
+@use '../styles/general.scss' as *;
 
 h4 {
   color: $primary;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,51 @@
 <script >
+
+
 export default {
     name: "Header"
 }
+
+
 </script>
 
-<template></template>
+<template>
+    <header>
+
+    </header>
+</template>
 
 <style lang="scss" scoped>
 //Scss relativo alla sola sezione di header
+
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+header {
+
+
+
+    ul {
+
+        text-align: right;
+
+        list-style: none;
+        margin: 2.5rem 1rem 4rem;
+        @include center(right);
+
+        li a {
+
+            display: inline-block;
+            padding: 1rem;
+            text-decoration: none;
+            font-weight: 600;
+            transition: background 0.3s;
+
+            &.active,
+            &:hover {
+                background: $secondary;
+            }
+        }
+    }
+
+}
 </style>
