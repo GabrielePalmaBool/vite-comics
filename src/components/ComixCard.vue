6d1bce51c32f696b759319f136df883bc98491a8
Gabriele <gabriele.palma.bool@gmail.com>
2023-12-22 15:35:09 +0100

    first proof of props

---
+++ src/components/ComixCard.vue
@@ -0,0 +1,26 @@
+<script >
+
+export default {
+    name: "ComixCard",
+
+    props: {
+        infoComic: Object
+    }
+}
+
+</script>
+
+<template>
+    <div class="CmxCard">
+        <img :src="infoComic.thumb" alt="">
+        <h3></h3>
+    </div>
+</template>
+
+<style scoped>
+.CmxCard {
+    width: 180px;
+
+    padding: 10px;
+}
+</style>
